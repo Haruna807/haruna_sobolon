d3d10566f2a2e9e06b9152c0b2db3e870f9182db
haruna <k.haruna1145@gmail.com>
2024-07-09 05:58:04 +0900

    ファーストビューの作成途中

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ハンバーガーアイコンを押したとき
+document.querySelector("#js-drawer-button").addEventListener("click", function(e) {
+  e.preventDefault();
+
+  document.querySelector("#js-drawer-button").classList.toggle("is-checked");
+  document.querySelector("#js-drawer-content").classList.toggle("is-checked");
+});
