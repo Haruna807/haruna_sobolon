a826d791af1a382c29c0dbbbcfbd9b0e5a70e09e
haruna <k.haruna1145@gmail.com>
2024-07-27 16:03:33 +0900

    スムーススクロールの実装

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ document.querySelector("#js-drawer-button").addEventListener("click", function(e
   document.querySelector("#js-drawer-button").classList.toggle("is-checked");
   document.querySelector("#js-drawer-content").classList.toggle("is-checked");
 });
+
+// ドロワーメニューのリンクが押された時、メニューが閉じる
+document.querySelectorAll('#js-drawer-content a[href^="#"]').forEach(function(link) {
+  link.addEventListener("click", function(e) {
+    document.querySelector("#js-drawer-button").classList.remove("is-checked");
+    document.querySelector("#js-drawer-content").classList.remove("is-checked");
+  });
+});
